073a3cb95fd7ca3f947f20b4e6c62ab44a4b0d3e
nico <nico@Terra.home>
2017-11-02 22:13:22 +0100

    Peer instances are now constructed from a bytestrin (ip+port) Replaced print() statements by logging.debug()

--- PyTo.py
+++ PyTo.py
@@ -1,14 +1,17 @@
-from BEncoding import bdecode, bencode
 import urllib.request
 import urllib.parse
 from hashlib import sha1
 import asyncio
+import logging
+
+from BEncoding import bdecode, bencode
 from Messages import *
 
 
-class peer:
-    def __init__(self, ip: str, port: int, bitfield_size: int):
-        self.ip = ip
+class Peer:
+    def __init__(self, buffer: bytes, bitfield_size: int):
+        ip_1, ip_2, ip_3, ip_4, port = unpack(">4BH", buffer)
+        self.ip = "{}.{}.{}.{}".format(ip_1, ip_2, ip_3, ip_4)
         self.port = port
         self.choking = True
         self.choked = True
@@ -16,17 +19,10 @@ class peer:
         self.bitfield = bytearray(b"\x00"*bitfield_size)
 
     def __repr__(self):
-        return "peer({}:{}, choking={}, choked={})".format(self.ip,
+        return "Peer({}:{}, choking={}, choked={})".format(self.ip,
                                                            self.port,
                                                            self.choking,
                                                            self.choked)
-
-    #TODO: useless ??
-    @classmethod
-    def from_compact_ipv4(cls, buffer, bitfield_size):
-        ip_1, ip_2, ip_3, ip_4, port = unpack(">4BH", buffer)
-        ipv4 = "{}.{}.{}.{}".format(ip_1, ip_2, ip_3, ip_4)
-        return peer(ipv4, port, bitfield_size)
 
 
 def _split(l: list, n: int) -> list:
@@ -39,17 +35,6 @@ def _split(l: list, n: int) -> list:
         chunks.append(l[i:i+n])
         i = i + n
     return chunks
-
-#TODO: duplicate
-def _bytes_to_ipv4_port(b: bytes) -> peer:
-    #print(b)
-    ipv4 = "{}.{}.{}.{}".format(int.from_bytes(b[0:1], "big"),
-                                int.from_bytes(b[1:2], "big"),
-                                int.from_bytes(b[2:3], "big"),
-                                int.from_bytes(b[3:4], "big"))
-    port = int.from_bytes(b[4:6], "big")
-    #print("{}:{}".format(ipv4, port))
-    return peer(ipv4, port)
 
 
 class Torrent:
@@ -101,24 +86,36 @@ class Torrent:
         }
 
         url = "{}?{}".format(self.announce.decode(), urllib.parse.urlencode(h))
-        print(url)
+        logging.debug(url)
         with urllib.request.urlopen(url) as response:
             d = bdecode(response.read())
-            self.peers = list(map(lambda b: peer.from_compact_ipv4(b, self.bitfield_size),
-                                  _split(d[b"peers"], 6)))
+            self.peers = list(map(lambda b: Peer(b, self.bitfield_size), _split(d[b"peers"], 6)))
+
+    def handle_message(self, message: Message, peer: Peer):
+        if isinstance(message, BitField):
+            if message.bitfield_size == self.bitfield_size:
+                peer.bitfield = bytearray(message.bitfield)
+            else:
+                raise ValueError("Invalid BitField message")
+        elif isinstance(message, Have):
+            try:
+                q, r = divmod(message.piece_index, 8)
+                peer.bitfield[q] = pow(2, r)
+            except IndexError:
+                raise ValueError("Invalid Have message")
 
 
-async def tcp_handshake(peer: peer, loop, torrent: Torrent):
-    header = b"[%r:%d] " % (peer.ip, peer.port)
+async def tcp_handshake(peer: Peer, loop, torrent: Torrent):
+    header = "[%r:%d] " % (peer.ip, peer.port)
     buffer_size = 4096
-    print(header + b"Connecting...")
+    logging.debug(header + "Connecting...")
 
     try:
         reader, writer = await asyncio.open_connection(peer.ip, peer.port, loop=loop)
-        print(header + b"Connected!")
+        logging.debug(header + "Connected!")
 
         message = HandShake(t.info_hash).to_bytes()
-        print(header + b"Send: " + message)
+        logging.debug(header + "Send: " + str(message))
         writer.write(message)
 
         data = await reader.read(buffer_size)
@@ -132,20 +129,20 @@ async def tcp_handshake(peer: peer, loop, torrent: Torrent):
         return
 
     try:
-        h = HandShake.from_bytes(data[0:68])
+        HandShake.from_bytes(data[0:68])
         buffer = data[68:]
 
     except ValueError:
-        print(header + b"Handshake failed")
+        logging.debug(header + "Handshake failed")
         writer.close()
-        print(header + b"Closed socket")
+        logging.debug(header + "Closed socket")
         return
 
     while True:
         message, buffer = Message.from_bytes(buffer)
         if message is not None:
-            print(header, buffer)
-            print((header + b"Received: "), message)
+            logging.debug(header + "Received: " + str(message))
+            torrent.handle_message(message, peer)
 
         try:
             data = await reader.read(buffer_size)
@@ -161,32 +158,18 @@ async def tcp_handshake(peer: peer, loop, torrent: Torrent):
             break
 
         buffer = buffer + data
-        print(message)
 
     writer.close()
-    print(header + b"Closed socket")
-
-    def handle_message(self, message: Message, peer: peer):
-        if isinstance(message, BitField):
-            if message.bitfield_size == self.bitfield_size:
-                peer.bitfield = bytearray(message.bitfield)
-            else:
-                raise ValueError("Invalid BitField message")
-        elif isinstance(message, Have):
-            try:
-                q, r = divmod(message.piece_index, 8)
-                peer.bitfield[q] = pow(2, r)
-                print(peer.bitfield)
-            except IndexError:
-                raise ValueError("Invalid Have message")
+    logging.debug(header + "Closed socket")
 
 
 if __name__ == '__main__':
-    # t = Torrent("./data/Torrent files/archlinux-2017.10.01-x86_64.iso.Torrent")
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
     t = Torrent("./data/Torrent files/archlinux-2017.10.01-x86_64.iso.Torrent")
-    print(t)
+    logging.debug(t)
     t.tracker_get()
-    print(t.peers)
+    logging.debug(t.peers)
 
     loop = asyncio.get_event_loop()
     coroutines = map(lambda p: tcp_handshake(p, loop, t), t.peers)
